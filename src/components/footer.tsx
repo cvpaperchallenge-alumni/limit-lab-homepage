0012d36d82f6f3eaf177281603e90a383b66f420
botany <yoshikikubotani.lab@gmail.com>
2024-12-25 09:09:12 +0000

    :art: Apply `npm run lint/format`

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { PiXLogo } from 'react-icons/pi'
 import { RxGithubLogo } from 'react-icons/rx'
-import { RiGlobalLine } from "react-icons/ri";
+import { RiGlobalLine } from 'react-icons/ri'
 import Link from 'next/link'
 import blackAlumniLogo from '../../public/alumni-logo-with-wide-black-trimmed.png'
 import whiteAlumniLogo from '../../public/alumni-logo-with-wide-white-trimmed.png'
@@ -25,58 +25,64 @@ export function Footer() {
   }, [theme])
 
   return (
-    <div className="h-44 w-full bg-secondary p-6 text-secondary-foreground flex justify-center">
-      <div className='w-full max-w-[1500px] flex flex-col items-center justify-between md:flex-row'>
+    <div className="flex h-44 w-full justify-center bg-secondary p-6 text-secondary-foreground">
+      <div className="flex w-full max-w-[1500px] flex-col items-center justify-between md:flex-row">
         {/* Placeholder logo */}
         <div className="flex flex-col items-center gap-1">
-          <div className="flex items-center space-x-2 mt-1 md:mt-0 md:ml-10">
+          <div className="mt-1 flex items-center space-x-2 md:ml-10 md:mt-0">
             <Avatar className="size-6">
               <AvatarImage src="https://via.placeholder.com/24" alt="Logo" />
               <AvatarFallback>AL</AvatarFallback>
             </Avatar>
             <span className="text-xl font-bold">LIMIT Lab</span>
           </div>
-          <div className="text-sub text-xxs md:hidden">
+          <div className="text-xxs text-sub md:hidden">
             © 2024 LIMIT Lab. All rights reserved.
           </div>
         </div>
 
-        <div className="hidden text-sub md:block md:text-s md:mt-0">
+        <div className="hidden text-sub md:mt-0 md:block md:text-s">
           © 2024 LIMIT Lab. All rights reserved.
         </div>
 
-        <div className='flex flex-col-reverse items-center gap-2 md:gap-0 md:flex-col md:items-start md:mr-10'>
-          <div className="text-xxxs md:text-xxs md:pl-2 flex gap-2">
+        <div className="flex flex-col-reverse items-center gap-2 md:mr-10 md:flex-col md:items-start md:gap-0">
+          <div className="flex gap-2 text-xxxs md:pl-2 md:text-xxs">
             Developed by{' '}
-            <span className='inline md:hidden'>
+            <span className="inline md:hidden">
               cvpaper.challenge alumni community
             </span>
           </div>
-          <div className='flex flex-col items-center gap-2 text-sub'>
+          <div className="flex flex-col items-center gap-2 text-sub">
             <Image
               alt="alumni logo"
-              className='hidden h-auto w-40 md:block'
+              className="hidden h-auto w-40 md:block"
               priority={true}
               sizes="100vw"
               src={isDarkMode ? whiteAlumniLogo : blackAlumniLogo}
             />
-            <div className='flex items-center gap-2'>
-              <Link href="https://github.com/cvpaperchallenge-alumni" target="_blank">
-                <RxGithubLogo className="size-5"/>
+            <div className="flex items-center gap-2">
+              <Link
+                href="https://github.com/cvpaperchallenge-alumni"
+                target="_blank"
+              >
+                <RxGithubLogo className="size-5" />
               </Link>
               <Separator
                 orientation="vertical"
                 className="hidden h-5 bg-muted-foreground md:block"
               />
               <Link href="https://twitter.com/cvpcalumni" target="_blank">
-                <PiXLogo className="size-5"/>
+                <PiXLogo className="size-5" />
               </Link>
               <Separator
                 orientation="vertical"
                 className="hidden h-5 bg-muted-foreground md:block"
               />
-              <Link href="https://note.com/gatheluck/n/nc469f2f35426" target="_blank">
-                <RiGlobalLine className="size-5"/>
+              <Link
+                href="https://note.com/gatheluck/n/nc469f2f35426"
+                target="_blank"
+              >
+                <RiGlobalLine className="size-5" />
               </Link>
             </div>
           </div>
