67737ab152101370da704220da71cad52e6c153d
botany <yoshikikubotani.lab@gmail.com>
2024-12-25 01:32:26 +0000

    :art: Use geistSans as  a font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
 import { ThemeProvider } from 'next-themes'
 import { ReactNode } from 'react'
+import { Geist, Geist_Mono } from 'next/font/google'
 
 import './globals.css'
+
+const geistSans = Geist({
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+})
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-background text-foreground">
+      <body className={`${geistSans.className} antialiased min-h-screen bg-background text-foreground`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
