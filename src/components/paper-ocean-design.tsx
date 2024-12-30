58e83dca393b90e2c9a281e451c6d6af3abab52b
botany <yoshikikubotani.lab@gmail.com>
2024-12-30 14:25:23 +0000

    :sparkles: finalized updates to paper ocean design after unsuccessful refinements

--- src/components/paper-ocean-design.tsx
+++ src/components/paper-ocean-design.tsx
@@ -1,32 +1,23 @@
 'use client'
 
-import { useState, useRef, use, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import Image from 'next/image'
 import testImage from '../../public/paper-ocean.png'
 
 export function PaperOceanDesign() {
   const [cursor, setCursor] = useState({ x: 0, y: 0 })
   const [freezedCursor, setFreezedCursor] = useState({ x: 0, y: 0 })
-  const [targetCursor, setTargetCursor] = useState({ x: 0, y: 0 })
   const [isIdle, setIsIdle] = useState(false)
   const [isStartMoving, setIsStartMoving] = useState(false)
-  const [isTracing, setIsTracing] = useState(false)
-  const [isDuringTracing, setIsDuringTracing] = useState(false)
   const idleTimer = useRef<NodeJS.Timeout | null>(null)
-  const [numTracing, setNumTracing] = useState(0)
 
   function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
-    if (isIdle && !isStartMoving) {
+    if (isIdle) {
       setFreezedCursor({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY })
       setIsStartMoving(true)
       setTimeout(() => {
         setIsStartMoving(false)
-        setIsTracing(true)
-      }, 200)
-    }
-    if (isStartMoving || !isDuringTracing) {
-      console.log('set')
-      setTargetCursor({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY })
+      }, 500)
     }
     setIsIdle(false)
     setCursor({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY })
@@ -35,20 +26,6 @@ export function PaperOceanDesign() {
       setIsIdle(true)
     }, 500)
   }
-
-  useEffect(() => {
-    if (isTracing && !isDuringTracing) {
-      setIsDuringTracing(true)
-      setTimeout(() => {
-        setNumTracing(numTracing + 1)
-        if (numTracing === 3) {
-          setIsTracing(false)
-          setNumTracing(0)
-        }
-        setIsDuringTracing(false)
-      }, 500)
-    }
-  }, [cursor, isTracing])
 
   return (
     <div className="relative h-64 min-w-80" onMouseMove={handleMouseMove}>
@@ -71,20 +48,14 @@ export function PaperOceanDesign() {
         style={{
           clipPath: isStartMoving
             ? `circle(30px at ${freezedCursor.x}px ${freezedCursor.y}px)`
-            : ( isTracing
-              ? `circle(30px at ${targetCursor.x}px ${targetCursor.y}px)`
-              : (
-                isIdle
-                ? `circle(50px at ${cursor.x}px ${cursor.y}px)`
-                : `circle(30px at ${cursor.x}px ${cursor.y}px)`
-              )
+            : (
+              isIdle
+              ? `circle(50px at ${cursor.x}px ${cursor.y}px)`
+              : `circle(30px at ${cursor.x}px ${cursor.y}px)`
             ),
           transition: isStartMoving
-            ? 'clip-path 0.2s'
-            : (isTracing
-              ? 'clip-path 0.5s'
-              : (isIdle ? 'clip-path 0.5s' : 'none')
-            ),
+            ? 'clip-path 0.5s'
+            : (isIdle ? 'clip-path 0.5s' : 'none'),
           transformOrigin: isStartMoving
             ? `${freezedCursor.x}px ${freezedCursor.y}px`
             : `${cursor.x}px ${cursor.y}px`,
